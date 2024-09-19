f9bf3005b1a2a55ad3a8efff091841b8b5027665
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-09-19 09:16:38 +0500

    6. task What century is it?

--- 6-kyu.js
+++ 6-kyu.js
@@ -1,12 +1,23 @@
-function digitalRoot(n) {
-    if (n < 10) return n;
-    let sum = 0;
-    while (n > 0) {
-        sum += n % 10;
-        n = Math.floor(n / 10);
-    }
-    return digitalRoot(sum);
+// function digitalRoot(n) {
+//     if (n < 10) return n;
+//     let sum = 0;
+//     while (n > 0) {
+//         sum += n % 10;
+//         n = Math.floor(n / 10);
+//     }
+//     return digitalRoot(sum);
+// }
+
+// // Test cases
+// console.log(digitalRoot(16));    
+
+
+function whatCentury(year) {
+    let century = Math.ceil(year / 100);
+    let suffix = (century % 10 == 1 && century % 100 != 11) ? 'st' :
+            (century % 10 == 2 && century % 100 != 12) ? 'nd' :
+            (century % 10 == 3 && century % 100 != 13) ? 'rd' : 'th';
+    return century + suffix;
 }
 
-// Test cases
-console.log(digitalRoot(16));    
+console.log(whatCentury("1234"))
