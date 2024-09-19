d01334b3865defa075dc937346352e0f2aff4678
ziyovuddintolipov <ziyovuddin.developer@gmail.com>
2024-09-19 10:38:48 +0500

    Angle Between Clock Hands

--- 6-kyu.js
+++ 6-kyu.js
@@ -12,12 +12,25 @@
 // console.log(digitalRoot(16));    
 
 
-function whatCentury(year) {
-    let century = Math.ceil(year / 100);
-    let suffix = (century % 10 == 1 && century % 100 != 11) ? 'st' :
-            (century % 10 == 2 && century % 100 != 12) ? 'nd' :
-            (century % 10 == 3 && century % 100 != 13) ? 'rd' : 'th';
-    return century + suffix;
-}
+// function whatCentury(year) {
+//     let century = Math.ceil(year / 100);
+//     let suffix = (century % 10 == 1 && century % 100 != 11) ? 'st' :
+//             (century % 10 == 2 && century % 100 != 12) ? 'nd' :
+//             (century % 10 == 3 && century % 100 != 13) ? 'rd' : 'th';
+//     return century + suffix;
+// }
 
-console.log(whatCentury("1234"))
+// console.log(whatCentury("1234"))
+// function handAngle(date) {
+//     const hours = date.getHours() % 12;
+//     const minutes = date.getMinutes();
+
+//     const hourAngle = (hours * 30) + (minutes / 2);
+//     const minuteAngle = minutes * 6;
+
+//     const angle = Math.abs(hourAngle - minuteAngle);
+//     return Math.min(angle, 360 - angle) * (Math.PI / 180);
+// }
+
+// const data = new Date();
+// console.log(handAngle(data));
